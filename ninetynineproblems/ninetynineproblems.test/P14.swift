f27d5dc5385869702d16b00a31a896943995ef3a
Thomas Cassany <thomas.cassany@ocado.com>
2018-02-28 21:47:50 +0000

    P14 done. 14/99

---
+++ ninetynineproblems/ninetynineproblems.test/P14.swift
@@ -0,0 +1,25 @@
+import XCTest
+
+//P14 (*) Duplicate the elements of a linked list.
+extension List {
+    func duplicate() -> List {
+        var buffer = [T]()
+        self.forEach { element in
+            buffer.append(element)
+            buffer.append(element)
+        }
+        return List(buffer)!
+    }
+}
+
+
+class P14Test: XCTestCase {
+    
+    func test() {
+        let list = List("a", "b", "c", "c", "d")!.duplicate()
+        let expectedList = List("a", "a", "b", "b", "c", "c", "c", "c", "d", "d")!
+        XCTAssertTrue(expectedList == list)
+
+    }
+}
+
