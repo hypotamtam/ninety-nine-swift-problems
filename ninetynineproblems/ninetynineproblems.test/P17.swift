7523240e576a34a131804b9359b50f92fd600eb1
Thomas Cassany <thomas.cassany@ocado.com>
2018-03-03 14:10:42 +0000

    P17 and P18 done. 16/99

--- ninetynineproblems/ninetynineproblems.test/P17.swift
+++ ninetynineproblems/ninetynineproblems.test/P17.swift
@@ -1,9 +1,39 @@
-//
-//  P17.swift
-//  ninetynineproblems.test
-//
-//  Created by Thomas Cassany on 03/03/2018.
-//  Copyright © 2018 hypotantam. All rights reserved.
-//
+import XCTest
 
-import Foundation
+//P17 (*) Split a linked list into two parts.
+//The length of the first part is given. Use a Tuple for your result.
+enum P17Error: Error {
+    case IndexOutOfBound
+}
+
+extension List {
+    func split(at index: Int) throws -> (left: List, right: List) {
+        guard index > 0 && length > index else {
+            throw P17Error.IndexOutOfBound
+        }
+        
+        var internalIndex = 0
+        var bufferRight = [T]()
+        var bufferLeft = [T]()
+        forEach { element in
+            if (internalIndex < index) {
+                bufferLeft.append(element)
+            } else {
+                bufferRight.append(element)
+            }
+            internalIndex += 1
+        }
+        
+        return (left: List(bufferLeft)!, right: List(bufferRight)!)
+    }
+}
+class P17Test: XCTestCase {
+    func test() {
+        let lists = try! List("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k")!.split(at: 3)
+        XCTAssertTrue(List("a", "b", "c")! == lists.left)
+        XCTAssertTrue(List("d", "e", "f", "g", "h", "i", "j", "k")! == lists.right)
+        
+        XCTAssertThrowsError(try List("a", "b", "c")?.split(at: 0))
+        XCTAssertThrowsError(try List("a", "b", "c")?.split(at: 3))
+    }
+}
--- ninetynineproblems/ninetynineproblems.test/P18.swift
+++ ninetynineproblems/ninetynineproblems.test/P18.swift
@@ -1,9 +1,43 @@
-//
-//  P18.swift
-//  ninetynineproblems.test
-//
-//  Created by Thomas Cassany on 03/03/2018.
-//  Copyright © 2018 hypotantam. All rights reserved.
-//
+import XCTest
 
-import Foundation
+//P18 (**) Extract a slice from a linked list.
+//Given two indices, I and K, the slice is the linked list containing the elements from and including the Ith element up to
+//but not including the Kth element of the original linked list. Start counting the elements with 0.
+enum P18Error: Error {
+    case IndexOutOfBound
+}
+
+extension List {
+    func slice(_ from: Int, _ to: Int) throws -> List {
+        guard (from < to) && (from >= 0) && (to <= length) else {
+            throw P18Error.IndexOutOfBound
+        }
+        
+        var index = 0
+        var buffer = [T]()
+        forEach { element in
+            if (index >= from) && (index < to) {
+                buffer.append(element)
+            }
+            index += 1
+        }
+        
+        return List(buffer)!
+    }
+}
+
+class P18Test: XCTestCase {
+    
+    func test() {
+        var list = try! List("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k")!.slice(3, 7)
+        XCTAssertTrue(List("d", "e", "f", "g")! == list)
+        list = try! List("a", "b", "c", "d")!.slice(0, 4)
+        XCTAssertTrue(List("a", "b", "c", "d")! == list)
+
+        XCTAssertThrowsError(try List("a", "b", "c", "d")!.slice(-1, 4))
+        XCTAssertThrowsError(try List("a", "b", "c", "d")?.slice(2, 5))
+
+        
+    }
+    
+}
